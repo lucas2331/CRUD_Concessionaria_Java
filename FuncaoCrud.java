8862a1516c013fff684055f3760ee597c2599d2e
LucasGomes <36939240+lucas2331@users.noreply.github.com>
2020-03-02 09:14:10 -0300

    Add files via upload

---
+++ FuncaoCrud.java
@@ -0,0 +1,75 @@
+package crudconcessionaria;
+
+public class AçõesCRUD {
+
+    String Codigo;
+    String Nome;
+    String Ano;
+    String Combustivel;
+    String Configuração;
+    String Lugares;
+    String Tração;
+
+    public String getCodigo(){
+        return Codigo;
+    }
+    
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+    
+    
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    
+    public String getAno() {
+        return Ano;
+    }
+
+    public void setAno(String Ano) {
+        this.Ano = Ano;
+    }
+
+    
+    public String getCombustivel() {
+        return Combustivel;
+    }
+
+    public void setCombustivel(String Combustivel) {
+        this.Combustivel = Combustivel;
+    }
+
+    
+    public String getConfiguração() {
+        return Configuração;
+    }
+
+    public void setConfiguração(String Configuração) {
+        this.Configuração = Configuração;
+    }
+
+    
+    public String getLugares() {
+        return Lugares;
+    }
+
+    public void setLugares(String Lugares) {
+        this.Lugares = Lugares;
+    }
+
+    
+    public String getTração() {
+        return Tração;
+    }
+
+    public void setTração(String Tração) {
+        this.Tração = Tração;
+    }
+    
+}
